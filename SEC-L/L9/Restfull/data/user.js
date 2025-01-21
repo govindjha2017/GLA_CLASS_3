0a5b6d6a6b66caf897a0bb23d68c398504a34b60
govind <govindkumarjha2017@gmail.com>
2025-01-21 08:50:50 +0530

    21-01-2025

---
+++ SEC-L/L9/Restfull/data/user.js
@@ -0,0 +1,25 @@
+const Users = [
+    {   
+        id:1,
+        name:'ajay',
+        email:'ajay123@gmail.com',
+        password:'abcd123',
+        city:'Delhi India'
+    },
+    {
+        id:2,
+        name:'raj',
+        email:'raj@gmail.com',
+        password:'raj123',
+        city:'Kolkata India'
+    },
+    {
+        id:3,
+        name:'rahul',
+        email:'rahul@gmail.com',
+        password:'rahul 123',
+        city:'Mumbai India'
+    },
+]
+
+module.exports = Users;
