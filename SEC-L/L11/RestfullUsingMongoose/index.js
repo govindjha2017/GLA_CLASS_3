e4b7a0146066c12d6a584f196f9d796c6d1940f7
govind <govindkumarjha2017@gmail.com>
2025-01-23 11:59:09 +0530

    23-01-2025

---
+++ SEC-L/L11/RestfullUsingMongoose/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine','ejs');
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/SEC-L-Restfull')
+  .then(() => console.log('DB Connected!'))
+  .catch(()=>{console.log('DB not conected')})
+
+
+const Product = require('./model/Product');
+
+app.get('/products',async (req,res)=>{
+    const products = await Product.find({});
+    res.render('product',{products})
+})
+
+
+
+app.listen(4000,()=>{
+    console.log('server run at port 4000')
+})
