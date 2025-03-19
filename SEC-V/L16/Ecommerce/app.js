6f5fc1499df797961ca136396e5a905b3803a5cb
govind <govindkumarjha2017@gmail.com>
2025-03-19 09:36:12 +0530

    19-03-3035

---
+++ SEC-V/L16/Ecommerce/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine','ejs')
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/ECOM-SEC-V')
+    .then(()=>{console.log('DB conected ')})
+    .catch(()=>{console.log('DB not conected')})
+
+// =====ROUTES
+
+const productRoutes = require('./router/product');
+app.use(productRoutes);
+
+const PORT = 5000;
+app.listen(PORT,()=>{
+    console.log('server run at port ',PORT)
+})
